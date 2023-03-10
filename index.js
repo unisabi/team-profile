bc7e4e6439d21b2486414b5c4dc9b1766856e9e4
Sabina Ceja-Siete <sabinasiete@gmail.com>
2023-03-09 19:44:55 -0800

    more index and added a small readme

--- index.js
+++ index.js
@@ -68,3 +68,135 @@ function appMenu() {
                 }
               });
           }
+          function addEngineer() {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'engineerName',
+                  message: "What is your engineer's name?",
+                  validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'Please enter at least one character.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'engineerId',
+                  message: "What is your engineer's id?",
+                  validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                      if (idArray.includes(answer)) {
+                        return 'This ID is already taken. Please enter a different number.';
+                      } else {
+                        return true;
+                      }
+                    }
+                    return 'Please enter a positive number greater than zero.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'engineerEmail',
+                  message: "What is your engineer's email?",
+                  validate: (answer) => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                      return true;
+                    }
+                    return 'Please enter a valid email address.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'engineerGithub',
+                  message: "What is your engineer's GitHub username?",
+                  validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'enter at least one character.';
+                  },
+                },
+              ])
+              .then((answers) => {
+                const engineer = new Engineer(
+                  answers.engineerName,
+                  answers.engineerId,
+                  answers.engineerEmail,
+                  answers.engineerGithub
+                );
+                teamMembers.push(engineer);
+                idArray.push(answers.engineerId);
+                createTeam();
+              });
+          }
+          function addIntern() {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'internName',
+                  message: "What is your intern's name?",
+                  validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'enter at least one character.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'internId',
+                  message: "What is your intern's id?",
+                  validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                      if (idArray.includes(answer)) {
+                        return 'This ID is already taken.Enter a different number.';
+                      } else {
+                        return true;
+                      }
+                    }
+                    return 'enter a greater number than zero.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'internEmail',
+                  message: "What is your intern's email?",
+                  validate: (answer) => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                      return true;
+                    }
+                    return 'enter a valid email address.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'internSchool',
+                  message: "What is your intern's school?",
+                  validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'enter at least one character.';
+                  },
+                },
+              ])
+              .then((answers) => {
+                const intern = new Intern(
+                  answers.internName,
+                  answers.internId,
+                  answers.internEmail,
+                  answers.internSchool
+                );
+                teamMembers.push(intern);
+                idArray.push(answers.internId);
+                createTeam();
+              });
+          }
