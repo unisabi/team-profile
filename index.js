002b22170e6a8c02a9b30406506745a12f1ee0ab
Sabina Ceja-Siete <sabinasiete@gmail.com>
2023-03-09 18:49:42 -0800

    more code on the index/template

--- index.js
+++ index.js
@@ -13,3 +13,58 @@ const idArray = [];
 console.log(
   '\nWelcome to the team gen!\nUse `npm run reset` to reset the dist/ folder\n'
 );
+function appMenu() {
+    function createManager() {
+      console.log('build your team :busts_in_silhouette:');
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate: (answer) => {
+              if (answer !== '') {
+                return true;
+              }
+              return 'enter at least one character.';
+            },
+          },
+          {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's id?",
+            validate: (answer) => {
+              const pass = answer.match(/^[1-9]\d*$/);
+              if (pass) {
+                return true;
+              }
+              return 'enter a positive number greater than zero.';
+            },
+          },
+          function createTeam() {
+            inquirer
+              .prompt([
+                {
+                  type: 'list',
+                  name: 'memberChoice',
+                  message: 'which type of team member would you like to add?',
+                  choices: [
+                    'Engineer',
+                    'Intern',
+                    "I don't want to add any more team members",
+                  ],
+                },
+              ])
+              .then((userChoice) => {
+                switch (userChoice.memberChoice) {
+                  case 'Engineer':
+                    addEngineer();
+                    break;
+                  case 'Intern':
+                    addIntern();
+                    break;
+                  default:
+                    buildTeam();
+                }
+              });
+          }
---
+++ reset.js
@@ -0,0 +1,6 @@
+// clears it out
+const fs = require('fs');
+if (fs.existsSync('./dist/team.html')) {
+  fs.unlinkSync('./dist/team.html');
+  console.log('/dist/ folder reset!');
+}
